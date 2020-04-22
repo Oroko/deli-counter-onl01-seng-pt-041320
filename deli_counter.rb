bba916614c87a173867c739874fef24f29bc682a
Abel Gechure <aoroko09@gmail.com>
2020-04-22 00:41:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,17 +6,23 @@
 katz_deli = []
 
 
-def line(array)
-  if array.count > 1
-  output = "The line is currently:"
-  array.each_with_index {|val, index| output << " #{index+1}. #{val}"}
-  puts output
-  else 
-    puts "The line is currently empty."
-end
-end
+# def line(array)
+#   if array.count > 1
+#   output = "The line is currently:"
+#   array.each_with_index {|val, index| output << " #{index+1}. #{val}"}
+#   puts output
+#   else 
+#     puts "The line is currently empty."
+# end
+# end
 
 def line(array)
   if array.count > 1 
-end
+    array.each_with_index do |name, index|
+      position = "#{name} is currently number #{index+1} on the line"
+      puts position
+    end
+  else
+    puts "The line is currently empty."
+  end
 end
