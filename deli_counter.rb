52f46573c9ee555ddcf9853ba5b122451d1b89f6
Abel Gechure <aoroko09@gmail.com>
2020-04-22 01:14:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,5 +19,5 @@ end
 def take_a_number(array, name)
   array.push(name)
   position = array.length
-  puts "Welcome, #{name}. You are number #{position} in line. "
+  "Welcome, #{name}. You are number #{position} in line. "
 end
